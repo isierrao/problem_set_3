d80d9e2b5935f9afab90d34ec5ddbdbccee1d35d
isierrao <i.sierrao@uniandes.edu.co>
2024-05-24 16:44:52 -0500

    modelos
    arboles de decisión.

--- scripts/modelos.R
+++ scripts/modelos.R
@@ -1,4 +1,4 @@
-p_load(caret)
+p_load(caret, neuralnet)
 
 
 
@@ -22,11 +22,12 @@ reg_lineal <- lm(price ~ surface_total + bedrooms +
      ascensor  + parqueo + patio +
      deposito  + distancia_escuela +
      distancia_estacion + distancia_comercial +
-     distancia_bus , data = train_1)
+     distancia_bus , data = train)
 
-predict_lm <- test_1 %>% mutate(price_lm = predict(reg_lineal, 
-                                newdata = test_1)) %>%
-    select(price_lm, price)
+predict_lm <- test %>% mutate(price = predict(reg_lineal, 
+                                newdata = test)) %>%
+    select(property_id, price)
+
   
 mae(predict_lm$price, as.numeric(predict_lm$price_lm))
 
@@ -44,9 +45,8 @@ model <- train(price ~ surface_total + bedrooms +
                  deposito + precio_por_mt2 + distancia_escuela +
                  distancia_estacion + distancia_comercial +
                  distancia_bus, data = train_1, 
-               method = "glmnet", 
-               trControl = train_control, 
-               tuneGrid = hyper_grid)
+               method = "glmboost", 
+               trControl = train_control)
 
 
 predict <- test_1 %>% mutate(price_lm = predict(model, 
@@ -54,3 +54,73 @@ predict <- test_1 %>% mutate(price_lm = predict(model,
   select(price_lm, price)
 
 mae(predict$price, as.numeric(predict$price_lm))
+
+
+
+model_pcr <- train(price ~ surface_total + bedrooms +
+                     property_type + lat + lon + 
+                     ascensor + parqueo +
+                     deposito + precio_por_mt2 + distancia_escuela +
+                     distancia_estacion + distancia_comercial +
+                     distancia_bus, 
+                   data = train_1, 
+                   method = "pcr", 
+                   trControl = train_control)
+
+
+predict_pcr <- test_1 %>% mutate(price_lm = predict(model_pcr, 
+                                                newdata = test_1)) %>%
+  select(price_lm, price)
+
+mae(predict_pcr$price, as.numeric(predict_pcr$price_lm))
+
+
+grid <- expand.grid(
+  n.trees = c(100, 200, 300),      # Número de árboles
+  interaction.depth = c(1, 3, 5),  # Profundidad de interacción
+  shrinkage = c(0.01, 0.1),        # Tasa de aprendizaje
+  n.minobsinnode = c(10, 20)       # Número mínimo de observaciones en nodos terminales
+)
+
+# Entrenar el modelo GBM
+model_gbm <- train(price ~ surface_total + bedrooms + 
+                     property_type + lat + lon + ascensor + parqueo + patio +
+                     deposito + precio_por_mt2 + distancia_escuela +
+                     distancia_estacion + distancia_comercial + distancia_bus, 
+                   data = train_1, 
+                   method = "gbm", 
+                   trControl = train_control, 
+                   tuneGrid = grid,
+                   verbose = FALSE)
+
+predict_pcr <- test_1 %>% mutate(price_lm = predict(model_gbm, 
+                                                    newdata = test_1)) %>%
+  select(price_lm, price)
+
+mae(predict_pcr$price, as.numeric(predict_pcr$price_lm))
+
+#red neuronal
+
+model_neuralnet <- train(price ~ surface_total + bedrooms + 
+                     property_type + lat + lon + ascensor + parqueo + patio +
+                     deposito + precio_por_mt2 + distancia_escuela +
+                     distancia_estacion + distancia_comercial + distancia_bus, 
+                   data = train_1, 
+                   method = "neuralnet", 
+                   trControl = train_control)
+# Chapinero ---------------------------------------------------------------
+
+limites <- getbb("Chapinero Bogota Colombia")
+
+test_chapinero <- test %>%
+  filter(
+    between(lon, limites[1, "min"], limites[1, "max"]) & 
+      between(lat, limites[2, "min"], limites[2, "max"])
+  )
+
+
+train_chapinero <- train %>%
+  filter(
+    between(lon, limites[1, "min"], limites[1, "max"]) & 
+      between(lat, limites[2, "min"], limites[2, "max"])
+  )
